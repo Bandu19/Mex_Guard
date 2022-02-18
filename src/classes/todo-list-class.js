d6e43d50e13a3b0e8547485b75cdef2dacaedd2e
Oscar Acuña <oscagustavo1961@gmail.com>
2022-02-17 19:44:14 -0600

    PROYECTO-MEXGUAR-HEADER(En desarrollo)

---
+++ src/classes/todo-list-class.js
@@ -0,0 +1,78 @@
+// EL PROCESO DE LA PAGINA (PARTE 2)
+
+import { Todo } from "./todo.class";
+
+export class TodoList {
+  constructor() {
+    // this.todos = [];
+    this.cargarLocalStorage();
+  }
+
+  nuevoTodo(todo) {
+    this.todos.push(todo);
+    // Despues de que insertamos al arreglo vacio
+    // se guardan los cambios
+    this.guardarLocalStorage();
+  }
+
+  eliminarTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id != id);
+    this.guardarLocalStorage();
+  }
+
+  //A LA PAR
+  marcarCompletado(id) {
+    for (const todo of this.todos) {
+      if (todo.id == id) {
+        todo.completado = !todo.completado;
+        this.guardarLocalStorage();
+        break;
+      }
+    }
+  }
+
+  eliminarCompletados() {
+    this.todos = this.todos.filter((todo) => !todo.completado);
+    this.guardarLocalStorage();
+  }
+
+  contarPendientes(completado) {
+    for (const todo2 of this.todos) {
+      // console.log("ARRAY EN TOTAL:", todo2.completado);
+      if (todo2.completado == !completado) {
+        let contador = 0;
+        contador++;
+        console.log("Veces:", contador);
+        break;
+      }
+    }
+  }
+
+  //-----------------> CLASE (115) <----------------------------
+
+  guardarLocalStorage() {
+    // Guardar en el LocalStorage en JSON tipo String el arreglo
+
+    //Para conservar tu arreglo en la carpeta(LocalStorage)
+    localStorage.setItem("todo", JSON.stringify(this.todos));
+  }
+
+  cargarLocalStorage() {
+    // Para conservar los datos en el LocalStorage(arreglo)
+    // aqui se conserva en HTML los cambios.
+    /***
+     * Al principio no existira dentro del localStorage
+     * el valor todo es por eso que toda la información
+     * sera almacenada dentro del "else"
+     */
+    if (localStorage.getItem("todo")) {
+      this.todos = JSON.parse(localStorage.getItem("todo")); // STRING a Object
+      console.log("cargarLocal:", this.todos);
+      console.log(typeof this.todos);
+    } else {
+      this.todos = [];
+    }
+
+    this.todos = this.todos.map((obj) => Todo.fromJson(obj));
+  }
+}
