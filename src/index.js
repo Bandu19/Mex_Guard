f9989c726f86a5a41ecd82ab5eb06ec92b748895
Oscar Acuña <oscagustavo1961@gmail.com>
2022-02-19 23:51:31 -0600

    PROYECTO-MEXGUAR-HEADER(Haciendo el menú responsive design)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import "./styles.css";
+// import "./styles.css";
+
+window.onscroll = () => {
+  //Datos que va almacenando en el scroll(nav)
+  /*El scroll se refiere al numero de pixeles
+  que recorremos hacia abajo*/
+  scroll = document.documentElement.scrollTop;
+  //Header de nuestra pagina web
+  header = document.getElementById("header");
+
+  if (scroll > 20) {
+    header.classList.add("nav_mod");
+  } else if (scroll < 20) {
+    header.classList.remove("nav_mod");
+  }
+};
