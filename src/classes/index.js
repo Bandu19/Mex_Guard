975fa55b43f427405b2367c3eb49f71954151eff
Oscar Acuña <oscagustavo1961@gmail.com>
2022-02-21 19:32:00 -0600

    Part 1

--- src/classes/index.js
+++
@@ -1,8 +0,0 @@
-//Control de importaciones
-// 1
-import { Todo } from "./todo.class";
-// 2
-import { TodoList } from "./todo-list-class";
-
-//EXPORTAR PARA QUE SEA LLAMADO
-export { Todo, TodoList };
