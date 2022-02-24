aba9a733038a0bbecd0ffc6402e2bf892fc77824
Oscar Acuña <oscagustavo1961@gmail.com>
2022-02-23 22:08:37 -0600

    Aplicando estilos Nav)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ window.onscroll = () => {
   scroll = document.documentElement.scrollTop;
   const header = document.querySelector(".content_ani");
   const h1 = document.querySelector(".color_letter");
+  const marco = document.querySelector(".items--cta");
+  const marco2 = document.querySelector(".items--cta2");
+  const marco3 = document.querySelector(".items--cta4");
 
   if (scroll > 30) {
     header.classList.add("nav_mod");
@@ -14,8 +17,14 @@ window.onscroll = () => {
   if (scroll > 500) {
     h1.classList.add("nav_h1");
     header.classList.add("nav_mod_int");
+    marco.classList.add("items--cta1");
+    marco2.classList.add("items--cta3");
+    marco3.classList.add("items--cta5");
   } else if (scroll < 500) {
     h1.classList.remove("nav_h1");
     header.classList.remove("nav_mod_int");
+    marco.classList.remove("items--cta1");
+    marco2.classList.remove("items--cta3");
+    marco3.classList.remove("items--cta5");
   }
 };
